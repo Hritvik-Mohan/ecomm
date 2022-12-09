bab182943c5d1c2a94420c992cf91b88ca95b6ed
Hritvik Mohan <hritvik5005@gmail.com>
2022-12-09 18:44:50 +0530

    Add JSON for cart and products

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
+import ProductCard from "../../components/Home/ProductCard/ProductCard";
 import Footer from "../../components/Shared/Footer/Footer";
 import Header from "../../components/Shared/Header/Header";
 
 function Home() {
+    // Creating a state for products data.
+    // whenever there is achange in products, it will force component refresh.
+    let [products, setProducts] = useState([]);
+    const [click, setClick] = useState(0);
+
+    // let data = "my data";
+
+    useEffect(() => {
+        fetch("Product.json" )
+        // calling json function
+        .then(res => res.json())
+        // listening for json function to return.
+        .then(res => {
+            setProducts(res);
+            console.log("Fetche called");
+        })
+    }, [click])
+
     return (
         <div>
             <Header/>
-            <div className="container">This is home Page</div>
+            <div class="container">
+                <h3>{ products && products.length }</h3>
+                <div className="row">
+                    {
+                        products.map((product) => (
+                            <div className="col-4">
+                                <ProductCard item={product}/>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
             <Footer/>
         </div>
     )
